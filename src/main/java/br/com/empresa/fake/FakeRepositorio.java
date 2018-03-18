005004f730e8487302af86079c57a9c7887f9d4e
sidnei <siddonfer@gmail.com>
2018-03-18 20:16:01 -0300

    adequando campos do retorno log

--- src/main/java/br/com/empresa/fake/FakeRepositorio.java
+++ src/main/java/br/com/empresa/fake/FakeRepositorio.java
@@ -1,6 +1,7 @@
 package br.com.empresa.fake;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,15 +25,12 @@ public class FakeRepositorio {
 
 	private static final Logger log = LoggerFactory.getLogger(FakeRepositorio.class);
 	private RestTemplate restTemplate = new RestTemplate();
-	private  final String HTTP_GTURNQUIST_QUOTERS_CFAPPS_IO_API_RANDOM = "http://gturnquist-quoters.cfapps.io/api/random";
+//	private  final String HTTP_GTURNQUIST_QUOTERS_CFAPPS_IO_API_RANDOM = "http://gturnquist-quoters.cfapps.io/api/random";
+	private  final String LOUIZZZ_AWS_API = "http://54.202.46.151:8080/logAplicacao";
 	
 	
 	
 	public String getGraficoTipoErros() {
-		Object forObject = restTemplate.getForObject(HTTP_GTURNQUIST_QUOTERS_CFAPPS_IO_API_RANDOM, Object.class);
-		log.info(forObject
-				.toString());
-		
 		
 		Map<String, Integer> map = new HashMap<String, Integer>();
 		map.put("Legenda1", 10);
@@ -79,20 +78,17 @@ public class FakeRepositorio {
 
 
 	public String getCarregamentoInicialTabela() {
-		LogErro l = new LogErro("1", "12/02/2018", "123.234.345", "123.234.345", "com.tabajara", "classe1", "metodo1", "123", "Exception", "asas", "asas");
-		LogErro l2 = new LogErro("2", "12/02/2018", "123.234.345", "123.234.345", "com.tabajara", "classe2", "metodo2", "234", "NullPointerException", "", "");
-		LogErro l3 = new LogErro("3", "13/02/2018", "123.234.345", "123.234.345", "com.tabajara.lalala", "classe3", "metodo3", "345", "Exception", "asas", "asas");
-		LogErro l4 = new LogErro("4", "14/02/2018", "123.234.345", "123.234.345", "com.tabajara", "classe1", "metodo1", "123", "Exception", "", "");
-		LogErro l5 = new LogErro("5", "15/02/2018", "123.234.345", "123.234.345", "com.tabajara", "classe2", "metodo2", "234", "NullPointerException", "assa", "");
-		LogErro l6 = new LogErro("6", "16/02/2018", "123.234.345", "123.234.345", "com.tabajara.lululu", "classe6", "metodo6", "3456", "Exception", "", "asas");
-		LogErro l7 = new LogErro("7", "12/02/2018", "123.234.345", "123.234.345", "com.tabajara", "classe1", "metodo1", "123", "Exception", "asas", "");
-		LogErro l8 = new LogErro("8", "12/02/2018", "123.234.345", "123.234.345", "com.tabajara", "classe2", "metodo2", "234", "NullPointerException", "", "assa");
-		LogErro l9 = new LogErro("9", "13/02/2018", "123.234.345", "123.234.345", "com.tabajara.lalala", "classe3", "metodo3", "345", "Exception", "", "");
-		LogErro l10 = new LogErro("10", "14/02/2018", "123.234.345", "123.234.345", "com.tabajara", "classe1", "metodo1", "123", "Exception", "", "");
-		LogErro l11= new LogErro("11", "15/02/2018", "123.234.345", "123.234.345", "com.tabajara", "classe2", "metodo2", "234", "NullPointerException", "", "");
-		LogErro l12= new LogErro("12", "16/02/2018", "123.234.345", "123.234.345", "com.tabajara.lilili", "classe6", "metodo6", "3456", "Exception", "", "");
-		List<LogErro> erros = Arrays.asList(l,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12);
 
+//		LogErro forObject = restTemplate.getForObject(LOUIZZZ_AWS_API, LogErro.class);
+		ResponseEntity<Object> forEntity = restTemplate.getForEntity(LOUIZZZ_AWS_API, Object.class);
+		
+		log.info(forEntity.toString());
+		
+
+
+		List<LogErro> erros = new ArrayList<LogErro>();
+		erros.add(new LogErro());
+		
 		DataTableBuilder dataTableBuilder = new DataTableBuilder(erros );
 		return dataTableBuilder.getCarregamentoInicialTabela();
 	}
